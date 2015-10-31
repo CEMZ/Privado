188ebcf2593ffa7cb94184efe9331e7ee2b74044
christian <christianmarin120490@gmail.com>
2015-10-30 23:22:09 -0600

    Index actualizado

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
   require_once('header.php');
   head("El divino maestro");
-
+?>
+	<div class="jumbotron">
+		<h1>Bienvenidos</h1>
+		<p>Este es el sistema de gesti&oacute;n para "El divino maestro", por favor inicie sesi&oacute;n.</p>
+		<p><a class="btn btn-primary btn-lg" href="login.php" role="button">Iniciar sesi&oacute;n</a></p>
+	</div>
+<?php
   footer();
 ?>
