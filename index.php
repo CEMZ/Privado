db28d0739886ad6c1a2f6dd4097144f0bd725080
christian <christianmarin120490@gmail.com>
2015-11-09 22:38:59 -0600

    terminado

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
   require_once('header.php');
   head("El divino maestro");
 ?>
-	<div class="jumbotron">
-		<h1>Bienvenidos</h1>
-		<p>Este es el sistema de gesti&oacute;n para "El divino maestro", por favor inicie sesi&oacute;n.</p>
-		<p><a class="btn btn-primary btn-lg" href="login.php" role="button">Iniciar sesi&oacute;n</a></p>
-	</div>
+
+  <div class="col-lg-12">
+      <h1>Administraci&oacute;n "El divino maestro"</h1>
+      <h3>Bienvenido <strong><?php echo $_SESSION['user']; ?></strong></h3>
+  </div>
+
 <?php
-  footer();
+  footer('');
 ?>
